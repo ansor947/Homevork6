8d00a4f6d68ec340ccc57c659f4103d6b0f2d056
Andrey Sorokin <ansorpy@gmail.com>
2022-10-28 18:39:46 +0300

    fix CREATE

--- CREATE.sql
+++ CREATE.sql
@@ -1,59 +1,5 @@
--- Здравствуйте, 
--- написал 2 варианта, так как не понимаю, что подойдет лучше. 
--- Если можете, подскажите какой и почему?
-
--- https://drive.google.com/file/d/15xs5DW2d7VTGY3UFziJsLGE5S0lRt0td/view?usp=sharing
--- таблицу обновил согласно рекомендации
-
-
-CREATE TABLE IF NOT EXISTS genre(
-    genre_id VARCHAR(40) PRIMARY KEY,
-    title VARCHAR (60) NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS musician(
-    musician_id VARCHAR(60) PRIMARY KEY,
-    names VARCHAR(80) NOT NULL 
-    );
-
-CREATE TABLE IF NOT EXISTS genres_musicians(
-    genre_id VARCHAR(40) REFERENCES genre(genre_id),
-    musician_id VARCHAR(60) REFERENCES musician(musician_id),
-    CONSTRAINT gm PRIMARY KEY(genre_id, musician_id)
-    );
-
-CREATE TABLE IF NOT EXISTS album(
-    album_id VARCHAR(40) PRIMARY KEY,
-    title_album VARCHAR (60) NOT NULL,
-    year_of_realise DATE NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS album_musicians(
-    album_id VARCHAR(40) REFERENCES album(album_id),
-    musician_id VARCHAR(60) REFERENCES musician(musician_id),
-    CONSTRAINT am PRIMARY KEY(album_id, musician_id)
-    );
-
-CREATE TABLE IF NOT EXISTS collection(
-    collection_id VARCHAR(40) PRIMARY KEY,
-    title_collection VARCHAR (60) NOT NULL,
-    year_of_realise DATE NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS track(
-    track_id VARCHAR(40) PRIMARY KEY,
-    album_id VARCHAR (60) NOT NULL UNIQUE REFERENCES album(album_id),
-    duration TIME,
-    collection_id VARCHAR (60) REFERENCES collection(collection_id)
-    );
-
-CREATE TABLE IF NOT EXISTS collections_tracks(
-    track_id VARCHAR(40) REFERENCES album(track_id),
-    collection_id VARCHAR(60) REFERENCES musician(collection_id),
-    CONSTRAINT am PRIMARY KEY(track_id, collection_id)
-    );
-
-    -----------------------------------------------------------
+-- https://drive.google.com/file/d/15xs5DW2d7VTGY3UFziJsLGE5S0lRt0td/view?usp=sharing 
+-- ссылка на диаграмму
 
 CREATE TABLE IF NOT EXISTS genre(
     id SERIAL PRIMARY KEY,
@@ -93,8 +39,7 @@ CREATE TABLE IF NOT EXISTS track(
     id SERIAL PRIMARY KEY,
     track_name VARCHAR (60) NOT NULL,
     album_id INTEGER REFERENCES album(id),
-    duration TIME,
-    collection_id INTEGER REFERENCES collection(id)
+    duration NUMERIC(3,2)
     );
 
 CREATE TABLE IF NOT EXISTS collections_tracks(
@@ -102,3 +47,28 @@ CREATE TABLE IF NOT EXISTS collections_tracks(
     track_id INTEGER REFERENCES album(id),
     collection_id INTEGER REFERENCES musician(id)
     );    
+
+Не могу понять как внести в таблицу данные NUMERIC. Прописал (3,2). Но получаю ошибку, 
+хотя из логики и найденного мной в интернете все верно должно быть. Постановка "," непомогает.
+Помогите, пожалуйста, понять как правильно должны быть прописаны данные при внесении!
+
+
+
+INSERT INTO track(track_name, duration)
+	VALUES
+	('Tell Me Why', '356'),
+	('Fifteen', '326'),
+	('Hotel California', '341'),
+	('What a Wonderful World', '201'),
+	('What Now My Love', '559'),
+	('Somewhere My Love', '412'),
+	('Hey Hey', '219'),
+	('Running on Faith', '542'),
+	('Follow My Lead', '312'),
+	('My Gun Goo', '530'),
+	('Ты не один', '325'),
+	('Дождь', '251'),
+	('Мой рок-н-ролл', '533'),
+	('Чёрное солнце', '601'),
+	('Детство', '305');
+
