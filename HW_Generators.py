4f9bfff6ffd501095f33c6a534ebac7ab822d81c
Andrey Sorokin <ansorpy@gmail.com>
2022-12-19 17:50:48 +0300

    gen

---
+++ HW_Generators.py
@@ -0,0 +1,75 @@
+import types
+
+from pprint import pprint
+
+from collections.abc import Iterable
+
+
+def flat_generator(list_of_lists):
+        for item_list in list_of_lists:
+            for item in item_list:
+
+                yield item
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+    pprint("Yes")
+
+
+#  Написать генератор аналогичный генератору из задания 2, 
+#  но обрабатывающий списки с любым уровнем вложенности. 
+
+
+
+def flat_generator(list_of_lists, ignore_types=(str, bytes)):
+    for item in list_of_lists:
+        if isinstance(item, Iterable) and not isinstance(item, ignore_types):
+            yield from flat_generator(item)
+        else:
+            yield item
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
+    pprint("Yes")
--- HW_Iterators.py
+++ HW_Iterators.py
@@ -1,39 +1,5 @@
 from pprint import pprint
 
-# Здравствуйте,
-# это решение я нашел в интернете. Оно работает и написано под задание №1 ДЗ. Если исходить из посыла в лекции,
-# что не стоит изобретать велосипед, а стоит погуглить, ибо все может быть предумано до меня, то я справился на 146%
-# Но в лекции было сказано, что этот инструмент будет необходим далее, поэтому огромная просьба посмотреть код, дать 
-# советы и вернуть работу на доработку, тем более, что задачу с yield еще не решил.
-
-
-class FlatIterator:
-    def __init__(self, lst):
-        self.lst = lst
-        self.cursor = -1
-        self.list_len = len(self.lst)
-
-    def __iter__(self):
-        self.cursor += 1
-        self.nest_cursor = 0
-        return self
-
-    def __next__(self):
-        if self.nest_cursor == len(self.lst[self.cursor]):
-          iter(self)
-        if self.cursor == self.list_len:
-          raise StopIteration
-        self.nest_cursor += 1     
-        return self.lst[self.cursor][self.nest_cursor - 1]
-
-for i in FlatIterator(list_of_lists_1):
-    pprint(i)
-
-# Это производное от моего решения решения, с понимание того, что я нашел в интернете. У меня нет желания копировать,
-# поэтому старался понять логику и заимствовать минимум. Впринципе, почти получилось. Не могу получить значение
-#  return строкой, а соответственно, собрать значения в виде одного списка и ловлю ошибку на второй проверке. 
-# Можете подсказать, как доработать это решение, если это возможно?
-
 class FlatIterator:
 
     def __init__(self, list_of_list):
@@ -47,10 +13,9 @@ class FlatIterator:
         return self
 
     def __next__(self):
-        # if len(self.list_of_list) == self.counter_values:
         if len(self.list_of_list[self.counter]) == self.counter_values:
             iter(self)
-        if self.counter > len(self.list_of_list):
+        if self.counter >= len(self.list_of_list):
             raise StopIteration
 
 
@@ -58,35 +23,6 @@ class FlatIterator:
         self.values = self.nested_lists[self.counter_values]
         self.counter_values += 1
         return self.values
-
-# Это решение было первым. Тут мне удалось пользуясь знакомыми инструментами, получить спиок значений в нужном виде,
-# но не удалось их распаковать. Мне не хватило знаний и найти подходящий способ в интернете я не смог. Можно
-# было использовать допфункцию, но в условии, в классе, прописаны только 3 Кроме того, не знаю, насколько уместен в 
-# решении цикл for. Если это уместно, подскажите, пожалуйста, как распаковать список?
-
-class FlatIterator:
-
-    def __init__(self, list_of_list):
-        self.list_of_list = list_of_list
-        
-
-    def __iter__(self):
-        self.counter = -1
-        return self
-
-    def __next__(self):
-        self.counter += 1
-        self.nested_lists = []
-        for item in self.list_of_list:
-            self.nested_lists.extend(item)
-
-            if self.counter == 1:
-            
-                raise StopIteration
-    
-        return self.nested_lists
-
-
 
 
 def test_1():
@@ -107,5 +43,5 @@ def test_1():
 
 
 if __name__ == '__main__':
-    pprint("Yes")
     test_1()
+    pprint("Yes")
